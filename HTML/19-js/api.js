9b4ccc9d058eaba790f79aa233717b1a95110ec9
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2025-09-23 18:56:57 +0200

    Update api.js

--- HTML/19-js/api.js
+++ HTML/19-js/api.js
@@ -1,2 +1,28 @@
-// API = Application Programing Interface
-// 
+/*
+
+API = Application Programing Interface
+aplikační programové rozhraní
+
+programové rozhraní (kód), který je určení pro ovládání
+určitého software (aplikace, web, systém)
+
+příklad 1)
+můžu mít API, které mi poskytl výrobce chytré domácnosti
+a já jako programátor si můžu naprogramovat nějaké vlastní chování
+této chytré domácnosti
+
+příklad 2)
+Facebook, IG, Twitter (X) mají API, pomocí, kterých mohu ovládat svůj účet
+- číst data, editovat profil, vytvořit nový post, atd ...
+
+příklad 3)
+můžu mít robota, který má API a můžu jej pomocí něho ovládat
+API může vypadat takto:
+robot = new Robot();
+robot.cameraOn();
+robot.goForward(100);
+robot.turnLeft();
+robot.goForward(10);
+robot.cameraOff();
+
+*/
