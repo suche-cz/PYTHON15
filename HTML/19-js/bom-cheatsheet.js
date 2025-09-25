04b10eb455b92cf87a2d68170b0c4dd224c3b2a6
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2025-09-25 18:24:44 +0200

    js bom + js cheatsheet

---
+++ HTML/19-js/bom-cheatsheet.js
@@ -0,0 +1,57 @@
+// BOM = Browser Object Model
+// Umožňuje komunikovat/ovládat okno/prohlížeč.
+
+// WINDOW
+window.innerWidth
+window.innerHeight
+window.scrollX
+window.scrollY
+window.open("url")
+window.close()
+window.scrollTo(0, 0)
+window.alert("Hi")
+window.confirm("Sure?")
+window.prompt("Enter")
+
+// NAVIGATOR
+navigator.userAgent
+navigator.language
+navigator.onLine
+navigator.geolocation.getCurrentPosition(pos => {...})
+
+// LOCATION (URL Adresa)
+location.href
+location.protocol(https)
+location.host(seznam.cz, '77.75.79.222')
+location.hostname(seznam.cz)
+location.port(443, 80)
+location.pathname('/watch', '/tag/ekonomika-29566')
+location.search('?type=new&search=text')
+location.hash('#') //kotva
+location.reload()
+location.assign("url")
+location.replace("url")
+
+// HISTORY
+// z důvodu soukromí nelze získat jednotivé stránky z historie
+history.back()
+history.forward()
+history.go(-2)
+history.pushState({ x: 1 }, "", "/new")
+history.replaceState({}, "", "/edit")
+
+
+// SCREEN - ?
+screen.width
+screen.height
+screen.availWidth
+screen.availHeight
+screen.orientation
+
+// TIMERS
+setTimeout(() => {...}, 1000)
+clearTimeout(id)
+setInterval(() => {...}, 1000)
+clearInterval(id)
+requestAnimationFrame(cb)
+cancelAnimationFrame(id)
---
+++ HTML/19-js/js-cheatsheet.js
@@ -0,0 +1,91 @@
+// VARIABLES
+let x = 1
+const y = 2
+var z = 3
+
+// DATA TYPES
+typeof 123        // "number"
+typeof "hi"       // "string"
+typeof true       // "boolean"
+typeof {}         // "object"
+typeof []         // "object"
+typeof null       // "object"
+typeof undefined  // "undefined"
+typeof (() => { })   // "function"
+
+// STRINGS
+"hi".length
+"hi".toUpperCase()
+"hi".includes("h")
+"hi".split("")
+"hi".replace("h", "y")
+
+// NUMBERS / MATH
+Math.round(4.6)
+Math.floor(4.9)
+Math.ceil(4.1)
+Math.max(1, 2, 3)
+Math.random()     // 0–1
+
+// FUNCTIONS
+function f(x) { return x * 2 }
+const g = x => x * 2
+
+// CONTROL FLOW
+if (x > 0) { } else { }
+switch (val) { case 1: break }
+
+// LOOPS
+for (let i = 0; i < 5; i++) { }
+while (x < 5) { }
+do { } while (x < 5)
+
+// ARRAYS
+arr = [1, 2, 3]
+arr.push(4)
+arr.pop()
+arr.shift()
+arr.unshift(0)
+arr.includes(2)
+arr.indexOf(3)
+arr.map(x => x * 2)
+arr.filter(x => x > 1)
+arr.reduce((a, b) => a + b, 0)
+arr.forEach(x => console.log(x))
+arr.find(x => x === 2)
+
+// OBJECTS
+obj = { a: 1, b: 2 }
+obj.a
+obj["b"]
+Object.keys(obj)
+Object.values(obj)
+Object.entries(obj)
+
+// JSON
+JSON.stringify({ a: 1 })
+JSON.parse('{"a":1}')
+
+// CLASSES
+class Person {
+    constructor(name) { this.name = name }
+    hi() { return "Hi " + this.name }
+}
+new Person("Tom").hi()
+
+// PROMISES / ASYNC
+fetch("/api")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
+
+async function load() {
+    try {
+        const res = await fetch("/api")
+        const data = await res.json()
+    } catch (e) { console.error(e) }
+}
+
+// MODULES
+export const xy = 1
+import { x } from "./file.js"
