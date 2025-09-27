ab2f6454cf50f7ce5066233227cfeb10ba99685e
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2025-09-27 09:05:56 +0200

    Create overview.js

---
+++ HTML/20-js/overview.js
@@ -0,0 +1,17 @@
+// JS (prohlížeč, server, ...)
+/*
+datové type
+operace (matematické, string)
+podmínky if (&&, ||, !)
+funkce
+cykly
+----
+složené datové typy
+třídy
+*/
+
+// DOM (pouze v prohlížeči)
+// Document Object Model
+
+// BOM (pouze v prohlížeči)
+// Browser Object Model
