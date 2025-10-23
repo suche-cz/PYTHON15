60bd02d91f7323f111c44242d9b2f841fac045f4
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2025-10-23 20:56:29 +0200

    JS 28

---
+++ HTML/28-js/oop.js
@@ -0,0 +1,34 @@
+// třída - class
+// - obecný popis chování
+// šablona pro tvorbu instancí
+// auto
+
+
+// instance - instance, object
+// - konkrétní výskyt třídy
+// moje auto
+
+/*
+
+typ mobil/produkt - třída
+produkt Apple Iphone 15
+produkt Apple Iphone 16
+produkt Apple Iphone 17
+produkt Apple Iphone 18
+
+
+Apple Iphone 18 - koncept
+Můj Apple Iphone 18 (barva, výrobní číslo...)
+Pepův Apple Iphone 18
+
+
+Vyrobce/Značky - třída
+Instance Apple
+Instance Samsung
+
+
+*/
+
+
+
+
