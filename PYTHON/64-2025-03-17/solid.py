a744ae9cde69a1dce5b0c3a7347ab19384f2c0a9
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2026-03-18 07:55:33 +0100

    add SOLID text, rename test file

--- PYTHON/64-2025-03-17/solid.py
+++ PYTHON/64-2025-03-17/solid.py
@@ -8,10 +8,17 @@ O - open-closed principle
 - otevřené pro změny, uzavřené pro modifikaci
 
 L - Liskov substituon principle
-- 
+- v OOP dědičnosti musí umět potomek nahradit 100% svého rodiče
 
 I - Interface Segregation Principle
+- v OOP dědičnosti by neměl rodič definovat zbytečná rozhraní, která na potomkovi nemají co dělat
+
 D - Dependency Inversion Principle (DIP)
+- závislosti by se měli dělat na obecných úrovních místo konkrétních
+
+
+https://gist.github.com/agusmakmun/b6701cd832d9e66955daa2b57957d8a2
+https://realpython.com/solid-principles-python/#dependency-inversion-principle-dip
 
 """
 
